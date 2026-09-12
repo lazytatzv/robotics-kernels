a360215455978294e30f84f1b80b79e44f1bc8e4
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-09-12 12:03:32 +0900

    Tuning options

--- build.sh
+++ build.sh
@@ -29,10 +29,52 @@ xzcat ../${RT_PATCH} | patch -p1 > /dev/null
 cp ../${TARGET}/.config .config
 
 # 4. Apply custom configurations
+# Base Real-Time configuration
 ./scripts/config --enable CONFIG_PREEMPT_RT
 ./scripts/config --set-str CONFIG_LOCALVERSION "-rt-robotics"
+
+# Timer & Scheduler Tuning (1000Hz, Full Tickless, RCU Offloading)
+./scripts/config --disable CONFIG_HZ_250
+./scripts/config --disable CONFIG_HZ_300
+./scripts/config --enable CONFIG_HZ_1000
+./scripts/config --set-val CONFIG_HZ 1000
+./scripts/config --enable CONFIG_NO_HZ_FULL
+./scripts/config --enable CONFIG_RCU_NOCB_CPU
+
+# CPU Performance Governor by default (minimize latency spikes)
+./scripts/config --disable CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE
+./scripts/config --disable CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL
+./scripts/config --enable CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE
+
+# Robotics & Industrial Communications (SocketCAN & USB-CAN adapters)
+./scripts/config --enable CONFIG_CAN
+./scripts/config --enable CONFIG_CAN_RAW
+./scripts/config --enable CONFIG_CAN_BCM
+./scripts/config --enable CONFIG_CAN_GW
+./scripts/config --enable CONFIG_CAN_DEV
+./scripts/config --enable CONFIG_CAN_CALC_BITTIMING
+./scripts/config --module CONFIG_CAN_GS_USB
+./scripts/config --module CONFIG_CAN_PEAK_USB
+./scripts/config --module CONFIG_CAN_SLCAN
+./scripts/config --module CONFIG_CAN_KVASER_USB
+
+# High-Precision Timing & Time Synchronization (PTP / IEEE 1588 for EtherCAT/TSN)
+./scripts/config --enable CONFIG_NETWORK_PHY_TIMESTAMPING
+./scripts/config --enable CONFIG_PTP_1588_CLOCK
+
+# Real-Time Latency Analysis & Tracers (rtla / cyclictest / timerlat)
+./scripts/config --enable CONFIG_FTRACE
+./scripts/config --enable CONFIG_SCHED_TRACER
+./scripts/config --enable CONFIG_HWLAT_TRACER
+./scripts/config --enable CONFIG_OSNOISE_TRACER
+./scripts/config --enable CONFIG_TIMERLAT_TRACER
+
+# Remove heavy debug locks and overhead for production RT
 ./scripts/config --disable CONFIG_DEBUG_INFO
 ./scripts/config --disable CONFIG_DEBUG_INFO_BTF
+./scripts/config --disable CONFIG_PROVE_LOCKING
+./scripts/config --disable CONFIG_LOCKDEP
+./scripts/config --disable CONFIG_DEBUG_ATOMIC_SLEEP
 ./scripts/config --set-str CONFIG_SYSTEM_TRUSTED_KEYS ""
 ./scripts/config --set-str CONFIG_SYSTEM_REVOCATION_KEYS ""
 
